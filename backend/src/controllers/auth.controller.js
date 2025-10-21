2e1d69cd04c3af03850fc7db2f1ba6282b976af9
Preetham T R <preethamtr72@gmail.com>
2025-10-21 19:55:48 +0530

    registration

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,57 @@
+const userModel=require("../models/user.model")
+const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
+
+async function registerUser(req,res){
+
+    const{fullName,email,password}=req.body;
+    
+    const isUserAlreadyExists=await userModel.findOne({
+        email
+    })
+
+    if(isUserAlreadyExists){
+        return res.status(400).json({
+            message:"User already exists"
+        })
+    }
+
+    const hashedPassword= await bcrypt.hash(password,10);
+
+    const user=await userModel.create({
+        fullName,
+        email,
+        password:hashedPassword
+    })
+
+    const token = jwt.sign({
+        id:user._id,
+
+    },"c6e4f85a1a9898e9d2f80fb28dbb2558")
+
+    res.cookie("token",token)
+
+    res.status(201).json({
+        message:"User registration successfully",
+        user:{
+            _id:user._id,
+            email:user.email,
+            fullName:user.fullName
+        }
+    })
+
+
+}
+
+async function loginUser(req,res) {
+    
+}
+
+module.exports={
+    registerUser,
+    loginUser,
+}
+
+
+
+
