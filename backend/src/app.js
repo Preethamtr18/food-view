e9342ea132d54a40f5490129a421d9455b999aa4
Preetham T R <preethamtr72@gmail.com>
2025-10-26 15:07:18 +0530

    restarted

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@
 const express=require('express');
 const cookieParser=require('cookie-parser')
 const authRoutes=require('./routes/auth.routes')
-
+const foodRoutes=require('./routes/food.routes')
 
 
 const app=express();
@@ -15,7 +15,8 @@ app.get("/",(req,res)=>{
     res.send("Hello world");
 })
 
-app.use('/api/auth',authRoutes)
+app.use('/api/auth',authRoutes);
+app.use('/api/food',foodRoutes);
 
 
 
